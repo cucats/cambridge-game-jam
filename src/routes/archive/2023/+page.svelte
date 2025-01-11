396dc4406f1c0e4bea464fbecfff6503db04f5c3
IntGrah <148660026+IntGrah@users.noreply.github.com.>
2025-01-11 19:15:00 +0000

    Rename files

--- src/routes/archive/2023/+page.svelte
+++ src/routes/archive/2023/+page.svelte
@@ -7,42 +7,42 @@
 
     const images = [
         {
-            src: "/2023/inferno_rising.png",
+            src: "/2023/inferno-rising.png",
             alt: "Inferno Rising",
             link: "https://firedemon111.itch.io/inferno-rising",
             title: "Inferno Rising",
             prize: "Popular Vote",
         },
         {
-            src: "/2023/mayi_garden.png",
+            src: "/2023/mayi-garden.png",
             alt: "Mayi Garden",
             link: "https://qiaozhi-lei.itch.io/mayi-garden",
             title: "MAYI Garden",
             prize: "Best for Theme",
         },
         {
-            src: "/2023/rhythm_mythril_fever.png",
+            src: "/2023/rhythm-mythril-fever.png",
             alt: "Rhythm Mythril Fever",
             link: "https://stolencheese.itch.io/rhythm-mythril-fever",
             title: "Rhythm Mythril Fever",
             prize: "Best Presentation",
         },
         {
-            src: "/2023/beaneth_the_earth.png",
+            src: "/2023/beaneth-the-earth.png",
             alt: "Beaneth the Earth",
             link: "https://mxbi.itch.io/beaneth-the-earth",
             title: "Beaneth the Earth",
             prize: "Best Gameplay",
         },
         {
-            src: "/2023/deck_of_dwarves.png",
+            src: "/2023/deck-of-dwarves.png",
             alt: "Deck of Dwarves",
             link: "https://joeoc.itch.io/deck-of-dwarves",
             title: "Deck of Dwarves",
             prize: "Most Original Idea",
         },
         {
-            src: "/2023/all_submissions.png",
+            src: "/2023/all-submissions.png",
             alt: "All Submissions",
             link: "https://itch.io/jam/camgamejam2023",
             title: "All Submissions",
