60fb553532d45078c1ea1eb03f84650252ba1a66
IntGrah <148660026+IntGrah@users.noreply.github.com.>
2025-01-11 16:29:00 +0000

    Add countdown

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,10 +1,14 @@
+<script>
+    import Countdown from "$lib/components/Countdown.svelte";
+</script>
+
 <div class="max-w-7xl mx-auto max-sm:text-center">
     <img
         src="/2025/logo.png"
         alt="Cambridge Game Jam Logo"
         class="mx-auto max-md:mb-4 sm:mr-8 size-60 sm:size-72 sm:float-left md:size-80"
     />
-    <h1 class="sm:pt-12 sm:text-5xl">Cambridge Game Jam 2025</h1>
+    <h1 class="sm:pt-12 sm:text-4xl">Cambridge Game Jam 2025</h1>
     <h3 class="sm:text-3xl">7th &ndash; 9th February</h3>
     <div class="mb-4 flex flex-wrap max-sm:justify-center gap-4">
         <a
@@ -24,4 +28,7 @@
     </div>
     <p class="text-xl">Build a game in 48 hours!</p>
     <p class="clear-left"></p>
+    <div class="m-auto">
+        <Countdown />
+    </div>
 </div>
